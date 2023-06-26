c6433487ce146810afe284e222abcb2ba5cf3955
Doru-George Teica <Doru-George.Teica@partner.bmw.de>
2023-06-26 22:06:59 +0200

    a

--- cegh_doru/pipelines.py
+++ cegh_doru/pipelines.py
@@ -24,6 +24,7 @@ class DownfilesPipeline(FilesPipeline):
             file_name = 'AT_day-ahead_' + str
         if 'trades' in request.url:
             file_name = 'AT_Within_Day_' + str
+
         if 'settlement' in request.url and 'Month' in request.url and 'front=1' in request.url:
             file_name = 'AT_futures_Month_front-period_1_' + str
         if 'settlement' in request.url and 'Month' in request.url and 'front=2' in request.url:
@@ -39,4 +40,19 @@ class DownfilesPipeline(FilesPipeline):
             file_name = 'AT_futures_Quarter_front-period_3_' + str
         if 'settlement' in request.url and 'Quarter' in request.url and 'front=4' in request.url:
             file_name = 'AT_futures_Quarter_front-period_4_' + str
+			
+       if 'settlement' in request.url and 'Season' in request.url and 'front=1' in request.url:
+            file_name = 'AT_futures_Season_front-period_1_' + str
+        if 'settlement' in request.url and 'Season' in request.url and 'front=2' in request.url:
+            file_name = 'AT_futures_Season_front-period_2_' + str
+        if 'settlement' in request.url and 'Season' in request.url and 'front=3' in request.url:
+            file_name = 'AT_futures_Season_front-period_3_' + str
+
+       if 'settlement' in request.url and 'Year' in request.url and 'front=1' in request.url:
+            file_name = 'AT_futures_Year_front-period_1_' + str
+        if 'settlement' in request.url and 'Year' in request.url and 'front=2' in request.url:
+            file_name = 'AT_futures_Year_front-period_2_' + str
+        if 'settlement' in request.url and 'Year' in request.url and 'front=3' in request.url:
+            file_name = 'AT_futures_Year_front-period_3_' + str
+
         return file_name
