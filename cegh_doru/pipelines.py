cf27511e3e08338d4a4807d06e3c55e4ccd84209
Doru-George Teica <Doru-George.Teica@partner.bmw.de>
2023-06-26 15:45:28 +0200

    a

--- cegh_doru/pipelines.py
+++ cegh_doru/pipelines.py
@@ -6,8 +6,15 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.pipelines.files import FilesPipeline
 
 
 class HelpDoruPipeline:
     def process_item(self, item, spider):
         return item
+
+
+class DownfilesPipeline(FilesPipeline):
+    def file_path(self, request, response=None, info=None):
+        file_name: str = request.url.split("/")[-1]
+        return file_name
