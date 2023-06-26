804e8d8f0417d4dae969488eeb68dd30abc7ef60
Doru-George Teica <Doru-George.Teica@partner.bmw.de>
2023-06-26 20:34:07 +0200

    a

---
+++ cegh_doru/spiders/cegh_monthly_futures_csv.py
@@ -0,0 +1,30 @@
+import scrapy
+
+from ..items import DownfilesItem
+from datetime import date
+
+
+class CeghCSVWithinDaySpider(scrapy.Spider):
+    name = "cegh_withinday_csv"
+	
+    def start_requests(self):
+        url = 'https://www.cegh.at/wp-admin/admin-ajax.php'
+        form_data={
+            'action': 'get_MonathlyFutures',
+            'period': 'Month'
+            'product': 'monthly',
+            'market': 'AT'
+        }
+        
+        yield scrapy.FormRequest(url,
+                                 formdata=form_data)
+
+    def parse(self, response):
+        file_url = response.css('a[dat-front="1"]::attr(href)').get()
+        file_url = response.urljoin(file_url)
+        item = DownfilesItem()
+        item['file_urls'] = [file_url]
+        today = date.today()
+        str = today.strftime("%d%m%Y")
+        item['original_file_name'] = 'AT_futures_Month_front-period_1_' + str
+        yield item
